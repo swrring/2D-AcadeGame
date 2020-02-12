3b2a18ddb89b8d3d3b3c514202c96e2005571fa8
Tony <swrring@gmail.com>
2020-02-13 01:16:14 +0900

    스테이지 타일맵 팔레트 설정 및 맵 구성 추가

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -6,11 +6,12 @@ public class PlayerMove : MonoBehaviour
 {
     // Start is called before the first frame update
     public float MaxSpeed;
-    public float MaxHight;
+    public float JumpPower;
     
     Rigidbody2D rigid;
     SpriteRenderer sprite;
     Animator anim;
+    BoxCollider2D gamemanager;
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
@@ -34,33 +35,56 @@ public class PlayerMove : MonoBehaviour
         {
             rigid.velocity = new Vector2(MaxSpeed*-1, rigid.velocity.y);
         }
-        if(rigid.velocity.y > MaxHight)
+        if(rigid.velocity.y > JumpPower)
         {
-            rigid.velocity = new Vector2(rigid.velocity.x, MaxHight);
+            rigid.velocity = new Vector2(rigid.velocity.x, JumpPower);
             
         }
+
+        //Ray cast 사용 예시
+        //Debug.DrawRay(rigid.position, Vector3.down,new Color(0, 1, 0));
+        RaycastHit2D rayhit = Physics2D.Raycast(rigid.position, Vector3.down, 1,LayerMask.GetMask("Tile"));
+        if (rigid.velocity.y < 0)
+        {
+            if (rayhit.collider != null)
+            {
+                if (rayhit.distance < 0.5f)
+                {
+                    anim.SetBool("isJumping", false);
+                }
+                //Debug.Log(rayhit.collider.name);
+            }
+        }
+        
         
     }
     private void Update()
     {
+        //점프 
+        if (Input.GetButtonDown("Jump") && !anim.GetBool("isJumping"))
+        {
+            if (Mathf.Abs(rigid.velocity.y) < 0.1f)
+            {
+                rigid.AddForce(Vector2.up * JumpPower, ForceMode2D.Impulse);
+                anim.SetBool("isJumping", true);
+            }
+        }
+
+
+
+
+
         //버튼 땔 때 정지 가속도 감소 추가
-        if(Input.GetButtonUp("Horizontal"))
+        if (Input.GetButtonUp("Horizontal"))
         {
 
-            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.1f, rigid.velocity.y);          
         }
 
         if (Input.GetButtonDown("Horizontal"))
         {
             sprite.flipX = Input.GetAxisRaw("Horizontal") == -1;           
-        }
-        if (Input.GetButtonDown("Jump"))
-        {
-            if (Mathf.Abs(rigid.velocity.y) < 0.1f)
-            {
-               rigid.AddForce(Vector2.up * MaxHight, ForceMode2D.Impulse);
-            }
-        }
+        }       
 
 
         if(Mathf.Abs(rigid.velocity.x) <= 0.4)
@@ -71,5 +95,7 @@ public class PlayerMove : MonoBehaviour
         {
             anim.SetBool("isWalking", true);
         }
+        
     }
+    
 }
